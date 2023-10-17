7e97fd84a72454facf0e3e1ea629cca1e7e4386b
zhangchi18 <zhangchi18@xiaomi.com>
2023-10-17 11:59:56 +0800

    Add optimized-images and the image generation script

---
+++ process-images.mjs
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
+
+// Dynamically import 'imagemin' and plugins
+const imagemin = await import('imagemin');
+const imageminJpegtran = await import('imagemin-jpegtran');
+const imageminPngquant = await import('imagemin-pngquant');
+
+// Recursively create directory if it doesn't exist
+function mkdirp(directory) {
+    if (!fs.existsSync(directory)) {
+        mkdirp(path.dirname(directory));
+        fs.mkdirSync(directory);
+    }
+}
+
+// Recursively process images in directory and subdirectories
+async function processImages(srcDir, destDir) {
+    const entries = fs.readdirSync(srcDir, { withFileTypes: true });
+
+    for (const entry of entries) {
+        const srcPath = path.join(srcDir, entry.name);
+        const destPath = path.join(destDir, entry.name);
+
+        if (entry.isDirectory()) {
+            // Recursively process this directory
+            await processImages(srcPath, destPath);
+        } else if (/\.(jpg|png|jpeg)$/i.test(entry.name)) {
+            // Process image
+            mkdirp(destDir);
+
+            // Resize image
+            await sharp(srcPath)
+                .resize({ width: 500, withoutEnlargement: true })
+                .toFile(destPath);
+
+            // Minify image
+            await imagemin.default([destPath], {
+                destination: destDir,
+                plugins: [
+                    imageminJpegtran.default(),
+                    imageminPngquant.default({ quality: [0.8, 0.85] })
+                ]
+            });
+        }
+    }
+}
+
+// Example usage
+processImages('images', 'optimized-images').then(() => {
+    console.log('All images processed');
+});
